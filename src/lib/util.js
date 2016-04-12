46534d2eefc5e8b90a9368030f08e177cd9d1442
Jon Dotsoy <hi@jon.soy>
2016-04-12 16:33:37 -0300

    Integrate utilapp

--- src/lib/util.js
+++ src/lib/util.js
@@ -1,33 +1,18 @@
+import {version as gVersion, help as gHelp} from 'utilapp'
 
-export let help = function ( {version, name, title, gitHead}, verbose, opts) {
-	let {shellInterative} = opts
-
-	let template =
-`
-Usage: ${name} {OPTIONS} [action]
-Version: ${getVersionStr(true, {version, title, gitHead}, verbose)}
-
-Options:
-
-    --verbose, -V    Shows more information.
-    --version, -v    Print NDeploy version. 
-    --help, -h       Show this messsage.
-
-Actions:
-`
-	console.log(template)
-}
-
-export let getVersionStr = function (short, {version, gitHead, title} , verbose) {
-	if (short) {
-		return `${version}${gitHead?` (${gitHead})`:''}`
-	} else {
-		return `${title} v${getVersionStr(true, {version, gitHead, title}, verbose)}`
-	}
+export let help = function (pkg, verbose, opts) {
+	console.log(gHelp(pkg, {
+		options: {
+			'--verbose, -V': 'Shows more information.',
+			'--version, -v': 'Print NDeploy version.',
+			'--help, -h': 'Show this messsage.',
+		},
+		arguments: {
+			'ab': 'abbbb',
+		}
+	}))
 }
 
 export let version = function ( pkg, verbose, opts ) {
-	console.log(getVersionStr(false, pkg, verbose))
+	console.log(gVersion(pkg))
 }
-
-
